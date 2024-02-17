655486d87c9753cfd5a1ca540af3fac503667999
Michealmiker <michealmiker@gmail.com>
2024-02-17 10:06:05 +0800

    Update test1

--- RunILasm.bat
+++ RunILasm.bat
@@ -2,4 +2,11 @@
 
 set ILASM_PATH="C:\Windows\Microsoft.NET\Framework64\v2.0.50727"
 
+for /F "tokens=1,2 delims=\\" %%a in ("%1") do (
+    set PATH=%%a
+    set FILENAME=%%b
+)
+
+mkdir built\%PATH%
+
 %ILASM_PATH%\ilasm.exe %1 /exe /output=%~dp0built\%1.exe
